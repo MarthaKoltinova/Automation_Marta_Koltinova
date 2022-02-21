15acc9552e03742accf5cdbde80d8219d7011ecf
MarthaKoltinova <96679318+MarthaKoltinova@users.noreply.github.com>
2022-02-21 11:01:53 +0300

    Add files via upload

---
+++ src/test/java/Lesson9/Lesson9.java
@@ -0,0 +1,37 @@
+package Lesson9;
+import BaseObjects.BaseTest;
+import PageObject.saucedemo.*;
+import org.testng.annotations.Test;
+import static javax.swing.UIManager.get;
+
+public class Lesson9 extends BaseTest{
+
+    @Test(priority = 1)
+    public void addItem() {
+        get(CatalogTestPage.class).addItem().goToShoppingCard().countOfItems();
+        get(BinTestPage.class).verifyPage();
+    }
+
+    @Test(priority = 2)
+    public void removeItem() {
+        get(BinTestPage.class).removeItem().verifyDelete().clickCheckout();
+        get(CheckoutPage.class).verifyPage();
+    }
+
+    @Test(priority = 3)
+    public void checkOutOrder() {
+        get(CheckoutPage.class).sendInformation("Marta", "koltinova", "1111");
+        get(Overview.class).verifyPage();
+    }
+
+    @Test(priority = 4)
+    public void overview() {
+        get(Overview.class).clickFinishButton();
+
+    }
+
+    @Test(priority = 5)
+    public void completeInformation() {
+        get(CompletePage.class).verifyPage();
+    }
+}
